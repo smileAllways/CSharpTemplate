8dd907af5a4e9280cfc060d46dffc31e266b40be
smileAllways <31954590+smileAllways@users.noreply.github.com>
2024-07-14 23:25:19 +0900

    취소 가능한 비동기 작업 템플릿
    CancellationToken 이용 비동기 기능 구현

---
+++ CancellableAsyncTasks/CancellableTemplateForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CancellableAsyncTasks
+{
+    partial class CancellableTemplateForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(520, 212);
+            this.textBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(520, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(376, 259);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "시작";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(457, 259);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "중지";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // CancellableTemplateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 288);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "CancellableTemplateForm";
+            this.Text = "CancellableTemplateForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ CancellableAsyncTasks/CancellableTemplateForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CancellableAsyncTasks
+{
+    /// <summary>
+    /// .net 4.8에서 작성하였습니다.
+    /// </summary>
+    public partial class CancellableTemplateForm : Form
+    {
+        /// <summary>
+        /// 취소 명령을 전달하기 위한 자원
+        /// </summary>
+        private CancellationTokenSource cancellationTokenSource;
+
+        public CancellableTemplateForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 취소 가능 비동기 함수
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private async Task CancellabeWorkAsync(CancellationToken cancellationToken)
+        {
+            // 아래와 같이 호출하면 중지되는 시점은
+            // 1. 작업 수행 전. (Task.Run에 전달되는 token 옵션)
+            // 2. MainWork 내부에서 전달 받은 token의 예외를 감지할 때
+            await Task.Run(() => MainWork(cancellationToken), cancellationToken);
+
+            textBox1.AppendText("Working is done\r\n");
+        }
+
+        /// <summary>
+        /// ★★★주 작업 지점★★★
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        private void MainWork(CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                // 취소 명령을 감지하고 'OperationCanceledException'을 발생시킨다.
+                // 예외는 현재 이곳이나 상위 어딘가에서 try-catch로 처리되어야 한다.
+                cancellationToken.ThrowIfCancellationRequested();
+
+                Invoke(new Action(() =>
+                {
+                    textBox1.AppendText($"Do Something : {i} loop\r\n");
+                }));
+
+                Thread.Sleep(500);
+            }
+        }
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            // 작업 취소 자원 할당(재사용이 불가능한 자원이므로, 신규로 할당해야한다.)
+            this.cancellationTokenSource = new CancellationTokenSource();
+
+            buttonStop.Enabled = true;
+            buttonStart.Enabled = false;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+
+            // 작업 취소로 발생하는 'OperationCanceledException'을 처리하는 지점.
+            try
+            {
+                await CancellabeWorkAsync(this.cancellationTokenSource.Token);
+            }
+            catch (OperationCanceledException ex) 
+            {
+                textBox1.AppendText("Working has been canceled\r\n");
+            }
+
+            buttonStop.Enabled = false;
+            buttonStart.Enabled = true;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (this.cancellationTokenSource!= null) 
+            { 
+                // 작업 취소 수행
+                this.cancellationTokenSource.Cancel();
+
+                // 자원 해제(필수)
+                this.cancellationTokenSource.Dispose();
+                this.cancellationTokenSource= null;
+            }
+        }
+    }
+}
